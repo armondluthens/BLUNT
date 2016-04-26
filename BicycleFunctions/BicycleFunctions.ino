4637bc5e60359cd9486f2663e9789af8a57523f3
Armond Luthens <armondluthens@gmail.com>
2016-04-26 13:37:43 -0500

    arduino code holding bike functions

---
+++ BicycleFunctions/BicycleFunctions.ino
@@ -0,0 +1,14 @@
+/*******************
+ * Bicycle Functions
+ *******************/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
